6dd1415b3c56313db305b95a9094a8cbe59bbe0d
MOJIO <mohae@kakao.com>
2025-06-10 21:12:43 +0900

    feat - 헤더 END

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
-import Header from './components/Header';
-
+import GlobalStyle from './styles/GlobalStyle';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <div className="App">
-        
-      </div>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          {/* 레이아웃 적용*/}
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          {/* 레이아웃 미적용*/}
+          {/* <Route path="/error" element={<Error404 />} /> */}
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
--- FrontEnd/src/components/Layout.jsx
+++ FrontEnd/src/components/Layout.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <Header />
-      <Content>{children}</Content>
+      <Content>
+        <Outlet />
+      </Content>
+      
     </>
   );
 };
+
+const Content = styled.main`
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 24px 16px;
+`;
 
 export default Layout;
